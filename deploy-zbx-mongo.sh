388dee78cd8a4e9175a7d3a52fc0a7b6e3a55a63
Shulepov Kirill <shulepovk@gmail.com>
2016-12-09 13:49:40 +0300

    fix permission

--- deploy-zbx-mongo.sh
+++ deploy-zbx-mongo.sh
@@ -1,5 +1,5 @@
 cp Scripts/{JSON.sh,mongodb_stat.sh} /etc/zabbix/
-chown .zabbix /etc/zabbix/{JSON.sh,mongodb_stat.sh}
+chown zabbix.zabbix /etc/zabbix/{JSON.sh,mongodb_stat.sh}
 
 echo "UserParameter       = mongodb_status,/etc/zabbix/mongodb_stat.sh" >> /etc/zabbix/zabbix_agentd.conf
 echo "UserParameter       = mongodb.discovery_db,/etc/zabbix/mongodb_stat.sh db" >> /etc/zabbix/zabbix_agentd.conf
